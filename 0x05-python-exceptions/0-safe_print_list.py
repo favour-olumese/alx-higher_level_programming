749d346422be1f3db3df5635c67c522dfd312aa2
favour-olumese <olumese-favor@gmail.com>
2022-10-11 04:39:42 +0000

    Task 0 and 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+        return x
+    except IndexError:
+        i = 0
+        for x in my_list:
+            print(x, end="")
+            i += 1
+        print()
+        return i
